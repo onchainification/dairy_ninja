402fa9ada99e711f694920995f2bff909c7cea64
Petrovska <petrovska@badger.com>
2023-11-18 16:19:52 +0300

    feat: fake a solver so we can settle order in tests

--- test/BaseFixture.sol
+++ test/BaseFixture.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 
 import {IChronicleOracle} from "../src/interfaces/IChronicleOracle.sol";
+import {IGPv2AllowListAuthentication} from "../src/interfaces/IGPv2AllowListAuthentication.sol";
 
 import {ComposableCoW} from "cow-order/ComposableCoW.sol";
 
@@ -16,6 +17,9 @@ contract BaseFixture is Test {
 
     ComposableCoW composableCow;
 
+    // fake solver agent
+    address constant FAKE_SOLVER = 0x5b47121521fBcAE0dFFfbC312Bb73fE88F4E8BE6;
+
     // gnosis safe address
     address constant GNOSIS_CHAIN_SAFE = 0xa4A4a4879dCD3289312884e9eC74Ed37f9a92a55;
 
@@ -25,6 +29,9 @@ contract BaseFixture is Test {
 
     // cowswap infra: https://docs.cow.fi/smart-contracts/introduction
     address constant COW_RELAYER = 0xC92E8bdf79f0507f65a392b0ab4667716BFE0110;
+
+    IGPv2AllowListAuthentication constant COW_ALLOW_LIST =
+        IGPv2AllowListAuthentication(0x2c4c28DDBdAc9C5E7055b4C863b72eA0149D8aFE);
 
     // NOTE: found addy in https://gnosisscan.io/address/0x5e16ca75000fb2b9d7b1184fa24ff5d938a345ef#readContract#F2
     address constant RELY_AUTH_ORACLE = 0xc50dFeDb7E93eF7A3DacCAd7987D0960c4e2CD4b;
@@ -40,5 +47,10 @@ contract BaseFixture is Test {
         composableCow = ComposableCoW(0xfdaFc9d1902f4e0b84f65F49f244b32b31013b74);
         // deploy handler
         orderHandler = new OrderHandler(composableCow);
+
+        // authorize the solver to fake orders
+        vm.prank(COW_ALLOW_LIST.manager());
+        COW_ALLOW_LIST.addSolver(FAKE_SOLVER);
+        assertTrue(COW_ALLOW_LIST.isSolver(FAKE_SOLVER));
     }
 }
