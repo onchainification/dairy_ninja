4809b418e27b60c1ae2b988b3efb8d14ccd0fa02
Petrovska <petrovska@badger.com>
2023-11-18 15:29:15 +0300

    chores: add cte WXDAI token for testing

--- test/BaseFixture.sol
+++ test/BaseFixture.sol
@@ -7,6 +7,8 @@ import {IChronicleOracle} from "../src/interfaces/IChronicleOracle.sol";
 
 import {ComposableCoW} from "cow-order/ComposableCoW.sol";
 
+import {IERC20} from "@openzeppelin/token/ERC20/IERC20.sol";
+
 import {OrderHandler} from "src/OrderHandler.sol";
 
 contract BaseFixture is Test {
@@ -15,6 +17,8 @@ contract BaseFixture is Test {
     ComposableCoW composableCow;
 
     address constant GNOSIS_CHAIN_SAFE = 0xa4A4a4879dCD3289312884e9eC74Ed37f9a92a55;
+
+    IERC20 constant WXDAI = IERC20(0xe91D153E0b41518A2Ce8Dd3D7944Fa863463a97d);
 
     // NOTE: found addy in https://gnosisscan.io/address/0x5e16ca75000fb2b9d7b1184fa24ff5d938a345ef#readContract#F2
     address constant RELY_AUTH_ORACLE = 0xc50dFeDb7E93eF7A3DacCAd7987D0960c4e2CD4b;
